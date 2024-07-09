bac29106243aae5c57d32fc7019a7f54216eb30f
spsiliras <104709495+spsiliras@users.noreply.github.com>
2024-07-09 11:03:03 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserInputComponent],
 })
 export class AppComponent {}
